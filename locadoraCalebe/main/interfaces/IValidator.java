98b3b26dd264810e300c3e119e7c413344a5890d
Calebe Lino <96066328+CalebeLino@users.noreply.github.com>
2023-01-31 10:03:10 -0300

    Update IValidator.java

--- locadoraCalebe/main/interfaces/IValidator.java
+++ locadoraCalebe/main/interfaces/IValidator.java
@@ -1,5 +1,6 @@
 package main.interfaces;
 
 public interface IValidator<T> extends ICRUDable{
+    // A ideia do IValidator herdar de ICRUDable era armazenar os validadores em um repositório para facilitar o uso dos services
     boolean validate(T required);
 }
