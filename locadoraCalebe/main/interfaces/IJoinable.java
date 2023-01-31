b0018b4c4718d3ab58f768e3df93cf9514bb1f9e
Calebe Lino <96066328+CalebeLino@users.noreply.github.com>
2023-01-31 09:52:31 -0300

    Comentario

--- locadoraCalebe/main/interfaces/IJoinable.java
+++ locadoraCalebe/main/interfaces/IJoinable.java
@@ -3,5 +3,6 @@ package main.interfaces;
 import main.enums.Cardinality;
 
 public interface IJoinable<T> {
+    //A ideia da interface IJoinable<T> era criar repositórios relacionais entre dois objetos
     Cardinality cardinality(); 
 }
